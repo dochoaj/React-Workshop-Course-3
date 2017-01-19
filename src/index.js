f6470144b1ec01cd41ea3d73efd289408cc30d48
Daniel Ochoa John <dochoajohn@gmail.com>
2017-01-19 11:38:32 -0300

    Add initial files

--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk'
-import reducers from './reducers'
 
 import App from './App'
 
-const store = createStore(reducers, applyMiddleware(thunk))
-
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <App />,
   document.querySelector('.container'),
 )
 
