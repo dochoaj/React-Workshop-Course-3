333777adb7629783c5491fcde12cadf4367041d0
Daniel Ochoa John <dochoajohn@gmail.com>
2017-01-19 11:40:27 -0300

    Incorporate redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import reducers from './reducers'
 
 import App from './App'
 
+const store = createStore(reducers, applyMiddleware(thunk))
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.querySelector('.container'),
 )
 
